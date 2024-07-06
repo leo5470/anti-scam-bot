e2f8daf2ea7e5121fad88e519ab9089c5efe5f33
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-07-06 11:51:10 +0800

    Update line bot sdk to v3

--- main.py
+++ main.py
@@ -1,66 +1,62 @@
-from fastapi import FastAPI, Request, HTTPException
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import TextSendMessage
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
 from openaiFunc import query_openai
-import os
 
-# the main entrypoint to use FastAPI.
-app = FastAPI()
+app = Flask(__name__)
+
+configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
-line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-# 指定函數處理來自/路徑的 POST 請求。
-# 使用 post 表示這個端點只接受HTTP POST方法的請
-# 求，這是API通常接收數據的方式。
-@app.post("/")
-# callback功能：用來回應特定事件或請求。
-async def callback(request: Request):
-    # FastAPI框架，回傳一個包含整個請求text的bytes
-    # await表示該方法可能需要一些時間完成，在此期間
-    # 非同步運行能去做其他工作。
-    body = await request.body() 
-    body = body.decode()
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-    # request.headers是一個字典，包含了所有http header資訊，
-    # .get('x-line-signature')嘗試獲取與x-line-signature相關的值。
-    # 與LINE平台互動，x-line-signature是個非常重要的字段，它包含
-    # 了一個簽名，她用於驗證請求確實是從LINE平台發送的，是一個安
-    # 全措施，用於確保接收到的數據的真實性與完整性。
-    x_line_signature = request.headers.get('x-line-signature')
-
-    # 處理來自LINE平台的Webhook請求並進行安全性檢查。
-    # try: 呼叫WebhookHandler的handle方法，該方法是
-    # line-bot-sdk 提供的方法，用於處理Webhook事件。
-    # handle 方法會使用 x_line_signature 來驗證 body 
-    # 的數字簽名是否有效。這是為了確保請求是從LINE平台
-    # 發出的，並且內容在傳輸過程中沒有被篡改。
-    # except: HTTPException 是FastAPI提供的一種特殊異常，
-    # 用於在HTTP請求中返回錯誤響應。這裡設置狀態碼為 403，
-    # 表示服務器理解請求但拒絕執行，並且詳細說明為 "Invalid 
-    # signature"，告知客戶端或開發者簽名驗證失敗的具體原因。 
+    # handle webhook body
     try:
-        handler.handle(body, x_line_signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-        raise HTTPException(status_code=403, detail="Invalid signature")
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-    # 取得 reply token 和訊息
-    # 等待request.json()非同步方法的完成。該方法將請求的text解析成字典。
-    # 詳細解釋在test_json.py
-    json_data = await request.json()
-    reply_token = json_data['events'][0]['replyToken']
-    user_message = json_data['events'][0]['message']['text']
+    return 'OK'
 
-    # 使用OpenAI GPT自動回覆
-    gpt_response = query_openai(user_message)
 
-    # 回應用戶訊息
-    line_bot_api.reply_message(
-        reply_token, # 指定回應發送到哪個對話
-        TextSendMessage(text=gpt_response)
-    )
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        incoming = event.message.text
+        ans = query_openai(incoming)
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=ans)]
+            )
+        )
 
-    # 返回一個字典，表明請求已經被正確處理。在FastAPI中，這會自動被轉換為JSON格式的HTTP響應，返回給調用方。
-    return {"message": "OK"}
+if __name__ == "__main__":
+    app.run()
--- openaiFunc.py
+++ openaiFunc.py
@@ -1,13 +1,12 @@
-from openai import AsyncOpenAI
-import asyncio
+from openai import OpenAI
 
-client = AsyncOpenAI(api_key="EMPTY", base_url="http://140.112.26.229:8000/v1/")
+client = OpenAI(api_key="EMPTY", base_url="http://140.112.26.229:8000/v1/")
 
 
-async def query_openai(prompt, systemPrompt="你是一名幫助人類的助手", top_p=0.7, max_tokens=512, temperature=0.95):
+def query_openai(prompt, systemPrompt="你是一名幫助人類的助手", top_p=0.7, max_tokens=512, temperature=0.95):
     # api_key = os.environ['OPENAI_API_KEY']
     
-    completion = await client.chat.completions.create(
+    completion = client.chat.completions.create(
         model="yentinglin/Llama-3-Taiwan-8B-Instruct",
         messages = [
             {
