c3fee5150f323bd677330ac8824f1af9afaf2f1c
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-07-06 12:22:37 +0800

    update default function

--- main.py
+++ main.py
@@ -28,20 +28,6 @@ handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 @app.route("/", methods=['POST', 'HEAD'])
 def default():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
-
     return 'OK'
 
 @app.route("/callback", methods=['POST'])
