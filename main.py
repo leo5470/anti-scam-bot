974deb116bf97c97225286f4fd8706c4af62d556
leo5470 <61446148+leo5470@users.noreply.github.com>
2024-07-07 13:00:33 +0800

    Complete, to be tested

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from linebot.v3.messaging import (
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
+    PushMessageRequest,
+    GroupSummaryResponse,
     TextMessage,
     TemplateMessage,
     CarouselTemplate,
@@ -24,10 +26,12 @@ from linebot.v3.webhooks import (
     JoinEvent
 )
 
-from openaiFunc import query_openai
+# from openaiFunc import query_openai
+from vertexAI import query_vertexAI
 from mongo import MongoDBClient
 
 import os
+from pathlib import Path
 
 client = MongoDBClient()
 
@@ -35,6 +39,10 @@ app = Flask(__name__)
 
 configuration = Configuration(access_token=os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+binaryPrompt = Path('./prompts/binary.txt').read_text()
+qaPrompt = Path('./prompts/qa.txt').read_text()
+suggestPrompt = Path('./prompts/suggest.txt').read_text()
 
 suggestFlag = False
 
@@ -61,20 +69,50 @@ def handle_add_friend(event):
     userID = event.source.user_id
     client.newUser(userID)
 
-# @handler.add(JoinEvent)
-# def handle_join_group(event):
-#     groupID = event.source.groupId
+@handler.add(JoinEvent)
+def handle_join_group(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        groupID = event.source.group_id
+        group_summary = line_bot_api.get_group_summary(id).to_dict
+        groupName = group_summary["groupName"]
+        client.insertGroup(groupID, groupName)
+
 
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+
         incomingMessage = event.message.text
-        # if(event.source.type == "group"):
+        userID = event.source.user_id
+        if(event.source.type == "group"):
+            groupID = event.source.group_id
+            init = client.initializeGroup(userID, groupID, incomingMessage)
+            if init:
+                messages = [TextMessage(text="初始化成功")]
+            else:
+                client.updateGroupHistory(userID, groupID, incomingMessage)
+                history = client.getGroupHistory(groupID=groupID)
+                resp = query_vertexAI(binaryPrompt, history, temperature=0)
+                if resp == "是":
+                    caution = "⚠️ 警告! 警告! 以上內容有點危險喔，可以點選選單內的”我該怎麼做”馬上了解下一步該做甚麼" # TODO: 警告訊息
+                    messages = [TextMessage(text=caution)]
+                    line_bot_api.push_message(
+                        PushMessageRequest(to=userID, messages=messages)
+                    )
+                return
             
 
-        if incomingMessage == "/提示":
-            groupNames = ["group 1", "group 2"]
+        elif incomingMessage == "/提示":
+            groupIDs = client.getGroupIDs(userID)
+            groupNames = []
+            for id in groupIDs:
+                group_summary = line_bot_api.get_group_summary(id).to_dict
+                name = group_summary["groupName"]
+                groupNames.append(name)
+
             columns = [
                 CarouselColumn(
                     thumbnail_image_url='https://cdn.prod.website-files.com/6030eb20edb267a2d11d31f6/63bd0e73eda88718096a822c_ConceptsLINEGroupCoverImage_a76a181134deb7c405b39e6803a648c8_2000.png',
@@ -95,14 +133,42 @@ def handle_message(event):
             template = CarouselTemplate(columns=columns)
             messages = [TemplateMessage(altText="Groups", template=template)]
             suggestFlag = True
-        else: 
-            if suggestFlag:
-                history = "你正在接受測試"
-                ans = query_openai(history) # TODO: change parameters
-            else:
-                ans = "請依照指示使用！"
+        elif suggestFlag:
+                groupName = incomingMessage
+                history = client.getGroupHistory(groupName=groupName)
+                if history == None:
+                    ans = "找不到指定群組，請再試一次！"
+                    messages = [TextMessage(text=ans)]
+                else:
+                    ans = query_vertexAI(suggestPrompt, history)
+                    client.updateSuggestHistory(userID, ans)
+                    columns = [
+                        CarouselColumn(
+                            thumbnail_image_url='https://cdn.prod.website-files.com/6030eb20edb267a2d11d31f6/63bd0e73eda88718096a822c_ConceptsLINEGroupCoverImage_a76a181134deb7c405b39e6803a648c8_2000.png',
+                            text=name,
+                            default_action=MessageAction(
+                                    label="進行提示詢問", # TODO: change
+                                    text="/提示詢問"
+                                ),
+                            actions=[
+                                MessageAction(
+                                    label="進行提示詢問", # TODO: change
+                                    text="/提示詢問"
+                                ),
+                            ]
+                        )
+                    ]
+                suggestFlag = False
+        elif incomingMessage == "/提示詢問":
+            suggest = client.getLatestSuggest(userID)
+            client.updateChatHistory(userID, assistant=suggest)
+            ans = "請開始詢問"
             messages = [TextMessage(text=ans)]
-        line_bot_api = MessagingApi(api_client)
+        else:
+            client.updateChatHistory(userID, user=incomingMessage)
+            history = client.getChatHistory(userID)
+            ans = query_vertexAI(qaPrompt, history)
+            messages = [TextMessage(text=ans)]
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
--- mongo.py
+++ mongo.py
@@ -3,6 +3,7 @@ from pymongo.mongo_client import MongoClient
 import os
 
 from dotenv import load_dotenv
+import json
 
 class MongoDBClient:
     def __init__(self):
@@ -16,6 +17,8 @@ class MongoDBClient:
         self.client = MongoClient(self.mongo_uri)
         self.db = self.client['anti-fraud']
         self.collection = self.db['data']
+        self.groupCollection = self.db['group']
+        self.suggestCollection = self.db['suggest']
         try:
             self.client.admin.command('ping')
             print("Pinged your deployment. You successfully connected to MongoDB!")
@@ -31,65 +34,151 @@ class MongoDBClient:
         "groups": {},
         "chat_history": []
         }
+        new_suggest_document = {
+            "user_id": userID,
+            "history": []
+        }
         result = self.collection.insert_one(new_document)
+        self.suggestCollection.insert_one(new_suggest_document)
         print(f"Inserted document ID: {result.inserted_id}")
     
     def findByUserID(self, userID):
         document = self.collection.find_one({"user_id": userID})
         return document
     
-    def updateDocument(self, userID, updated_document):
+    def findByGroupID(self, groupID):
+        document = self.groupCollection.find_one({"group_id": groupID})
+        return document
+    
+    def findByGroupName(self, groupName):
+        document = self.groupCollection.find_one({"group_name": groupName})
+        return document
+    
+    def getGroupOwner(self, groupID):
+        document = self.groupCollection.find_one({"group_id": groupID})
+        return document["owner"]
+    
+    def getGroupsInfo(self, userID):
+        document = self.findByUserID(userID)
+        group_keys = list(document["groups"].keys())
+        infoList = []
+        for key in group_keys:
+            groupDocument = self.groupCollection.find_one({"group_id": key})
+            infoList.append({groupDocument["group_id"]: groupDocument["group_name"]})
+        return group_keys
+    
+    def getGroupHistory(self, groupName=None, groupID=None):
+        if groupName != None:
+            document = self.findByGroupName(groupName)
+        else:
+            document = self.findByGroupName(groupID)
+        if document == None:
+            return None
+        userID = document["owner"]
+        groupID = document["group_id"]
+        user_document = self.findByUserID(userID)
+        chatList = user_document["groups"][groupID]
+        chatList_json = json.dumps(chatList)
+        return chatList_json
+    
+    def getChatHistory(self, userID):
+        document = self.findByUserID(userID)
+        if document == None:
+            return None
+        chatList = document["chat_history"]
+        chatList_json = json.dumps(chatList)
+        return chatList_json
+    
+    def getLatestSuggest(self, userID):
+        document = self.suggestCollection.find_one({"user_id": userID})
+        return document["history"][-1]
+    
+    def updateUserDocument(self, userID, updated_document):
         self.collection.update_one({"user_id": userID}, {"$set": updated_document})
     
-    def insertGroup(self, userID, groupID):
-        document = self.findByUserID(userID)
+    def insertGroup(self, groupID, groupName):
+        document = self.findByGroupID(groupID)
+        if document != None:
+            return
+        new_document = {
+            "group_id": groupID,
+            "group_name": groupName,
+            "owner": "",
+            "initialized": False
+        }
+        self.groupCollection.insert_one(new_document)
+
+    def initializeGroup(self, userID, groupID, initMessage):
+        groupDocument = self.groupCollection.find_one({"group_id": groupID})
+        if groupDocument == None:
+            raise Exception("Group has not been inserted.")
+        if groupDocument["initialized"]:
+            return False # 正常情況，當作每次進行進行一次(應急作法)
+        groupDocument["initialized"] = True
+        groupDocument["owner"] = userID
+        self.groupCollection.update_one({"groupID": groupID}, {"$set": groupDocument})
+        document = self.collection.find_one({"user_id": userID})
         document["groups"].update(
             {
-                groupID: []
+                groupID: [
+                    {
+                        "role": "user",
+                        "content": initMessage
+                    }
+                ]
             }
         )
-        self.updateDocument(userID, document)
+        self.updateUserDocument(userID, document)
+        return True
     
-    def updateChatHistory(self, userID, user, assistant):
-        document = self.findByUserID(userID)
-        document["chat_history"].append(
-            {
-                "role": "user",
-                "content": user
-            }
-        )
-        document["chat_history"].append(
-            {
-                "role": "assistant",
-                "content": assistant
-            }
-        )
-        self.updateDocument(userID, document)
     
-    def updateGroupHistory(self, userID, groupID, user=None, counter=None):
+    def updateChatHistory(self, userID, user: None, assistant: None):
         document = self.findByUserID(userID)
-        if user:
-            document["groups"][groupID].append(
+        if user != None:
+            document["chat_history"].append(
                 {
                     "role": "user",
                     "content": user
                 }
             )
-        if counter:
+        if assistant != None:
+            document["chat_history"].append(
+                {
+                    "role": "assistant",
+                    "content": assistant
+                }
+            )
+        self.updateUserDocument(userID, document)
+    
+    def updateGroupHistory(self, userID, groupID, message):
+        document = self.findByUserID(userID)
+        groupDocument = self.findByGroupID(groupID)
+        if groupDocument["owner"] == userID:
+            document["groups"][groupID].append(
+                {
+                    "role": "user",
+                    "content": message
+                }
+            )
+        else:
             document["groups"][groupID].append(
                 {
                     "role": "counterpart",
-                    "content": counter
+                    "content": message
                 }
             )
-        self.updateDocument(userID, document)
+        self.updateUserDocument(userID, document)
     
+    def updateSuggestHistory(self, userID, suggest):
+        document = self.suggestCollection.find_one({"user_id": userID})
+        document["history"].append(suggest)
+
     def clearChatHistory(self, userID):
         document = self.findByUserID(userID)
         document["chat_history"] = []
-        self.updateDocument(userID, document)
+        self.updateUserDocument(userID, document)
     
     def clearGroupHistory(self, userID, groupID):
         document = self.findByUserID(userID)
         document["groups"][groupID] = []
-        self.updateDocument(userID, document)
+        self.updateUserDocument(userID, document)
